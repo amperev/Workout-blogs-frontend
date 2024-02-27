0c35b56564ab74ac61217b49c4f029047f4f8de7
amperev <skt644@gmail.com>
2024-02-27 18:13:38 +0530

    Add WorkoutContext and useWorkoutsContext

--- src/components/WorkoutForm.js
+++ src/components/WorkoutForm.js
@@ -1,7 +1,9 @@
 import {useState} from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
 
 const WorkoutForm = () => {
+    const {dispatch}=useWorkoutsContext()
     const [title,setTitle] =useState("")
     const [load,setLoad] =useState("")
     const [reps,setReps] =useState("")
@@ -33,6 +35,7 @@ const WorkoutForm = () => {
             setTitle('');
             setLoad('');
             setReps('');
+            dispatch({type:'CREATE_WORKOUT', payload: json})
         }
     }
 
