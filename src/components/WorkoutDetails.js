46e31110eb0a8ac0496b88a85e68bfb96e3aed56
amperev <skt644@gmail.com>
2024-02-27 20:06:27 +0530

    Add date-fns library and format workout creation date

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -1,5 +1,6 @@
-import React from 'react'
 import { useWorkoutsContext} from '../hooks/useWorkoutsContext'
+import  formatDistanceToNow  from "date-fns/formatDistanceToNow";
+
 function WorkoutDetails({workout}) {
 
   const {dispatch}= useWorkoutsContext()
@@ -18,7 +19,7 @@ function WorkoutDetails({workout}) {
         <h4>{workout.title}</h4>
         <p><strong>Load (kg):</strong> {workout.load}</p>
         <p><strong>Reps:</strong> {workout.reps}</p>
-        <p>{workout.createdAt}</p>
+        <p>{formatDistanceToNow(new Date(workout.createdAt),{addSuffix:true})}</p>
         <span className="material-symbols-outlined" onClick={handleClick}>delete</span> 
     </div>
   )
