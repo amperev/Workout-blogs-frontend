23b292649a709b998f68a4e0b8aaf4c7daad2eac
amperev <skt644@gmail.com>
2024-03-04 19:49:57 +0530

    Add login, logout, and signup functionality

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,14 +1,16 @@
 import {useState} from 'react'
+import useSignup from '../hooks/useSignup'
 
 const Signup = () => {
     const [email,setEmail] =useState('')
     const [password,setPassword]= useState('')
-
+    const {signup,error,isLoading} =useSignup()
     const handleSubmit= async (e)=>{
         e.preventDefault();
 
-        console.log(email, password)
+        await signup(email,password)
     }
+    
   return (
     <form className='signup' onSubmit={handleSubmit}>
         <h3>Sign up</h3>
@@ -25,7 +27,8 @@ const Signup = () => {
          onChange={(e)=> setPassword(e.target.value)}
          />
 
-         <button >Sign Up</button>
+         <button disabled={isLoading}>Sign Up</button>
+         {error && <div className='error'>{error}</div> }
     </form>
   )
 }
