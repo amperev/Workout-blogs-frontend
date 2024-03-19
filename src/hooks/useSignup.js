59ee7ca353cf85f1d12d368a67af2f48628727fa
amperev <skt644@gmail.com>
2024-03-20 01:25:25 +0530

    Refactor useSignup hook to handle errors and loading states properly

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,43 +1,43 @@
 import { useState } from "react";
-import {useAuthContext} from './useAuthContext'
-
+import { useAuthContext } from "./useAuthContext";
 
 const useSignup = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const { dispatch } = useAuthContext();
 
-    const [error,setError]= useState(null)
-    const [isLoading, setIsLoading] = useState(null)
-    const {dispatch} =useAuthContext()
+  const signup = async (email, password) => {
+    setIsLoading(true);
+    setError(null);
 
-    const signup = async(email,password)=>{
-        setIsLoading(true);
-        setError(null);
+    try {
+      const response = await fetch("/api/user/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
 
-        const response = await fetch('/api/user/signup',{
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify({email,password})
-        })
+      const json = await response.json();
 
-        const json= await response.json();
+      if (!response.ok) {
+        setIsLoading(false);
+        setError(json.error);
+      }
+      if (response.ok) {
+        localStorage.setItem("user", JSON.stringify(json));
 
-        if(!response.ok){
-            setIsLoading(false);
-            setError(json.error)
-        }
-        if(response.ok){
-            
-            localStorage.setItem('user',JSON.stringify(json))
+        dispatch({ type: "LOGIN", payload: json });
 
-            dispatch({type:'LOGIN',payload:json})
-
-            setIsLoading(false);
-
-
-        }
+        setIsLoading(false);
+      }
+    } catch (error) {
+      setError(error.message);
+      console.log(error.message);
     }
-  return {signup,isLoading,error}
-}
+  };
+  return { signup, isLoading, error };
+};
 
-export default useSignup
+export default useSignup;
